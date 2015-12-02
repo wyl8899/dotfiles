e2b3327ce536da94f08b9404a5bc85a7e583a09d
wyl8899 <wyl8899k@gmail.com>
2015-12-02 14:53:28 +0800

    goodbye HOME

--- HOME/bin/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HOME_DIR=/home/wyl8899
-BIN_DIR=$HOME_DIR/bin
-$BIN_DIR/wylxcape.sh
-$BIN_DIR/wylcaps.sh
--- HOME/bin/wylxcape.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xcape -e 'Control_L=Escape'
-xcape -e 'Caps_Lock=Escape'
