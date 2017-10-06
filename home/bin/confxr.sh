19b332efb05c123361d078202c5e793b607399fa
wyl8899 <wyl8899k@gmail.com>
2017-10-06 15:41:59 +0800

    sync

--- home/bin/confxr.sh
+++ home/bin/confxr.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-cmd1="auto"
-cmd2="off"
-if [ -n "$1" ]; then
-	cmd1=$1
-	if [ -n "$2" ]; then
-		cmd2=$2
-	fi
+c_eDP1="--auto"
+c_DP1="--off"
+c_DP2="--off"
+if [[ "$1" == "v" ]]; then
+  c_eDP1="--off"
+  c_DP1="--off"
+  c_DP2="--auto"
+elif [[ "$1" == "m" ]]; then
+  c_eDP1="--off"
+  c_DP2="--mode 1920x1080 --rotation normal"
+  c_DP1="--mode 1920x1080 --rotation normal --right-of DP2"
+elif [[ "$1" == "mv" ]]; then
+  c_eDP1="--off"
+  c_DP1="--mode 1920x1080 --rotation normal --pos 1080x300"
+  c_DP2="--mode 1920x1080 --rotation left --pos 0x0"
 fi
-cmd="xrandr --output eDP1 --$cmd1 --output DP2 --$cmd2"
+cmd="xrandr --output eDP1 $c_eDP1 --output DP2 $c_DP2 --output DP1 $c_DP1"
 echo $cmd
 $cmd
--- home/bin/wylmouse.sh
+++ home/bin/wylmouse.sh
@@ -2,4 +2,5 @@
 xset m default
 xset m 0
 xinput set-float-prop "Lenovo Precision USB Mouse" "Device Accel Constant Deceleration" 0.75
+xinput set-float-prop "Razer Razer DeathAdder" "Device Accel Constant Deceleration" 1.13
 xset m default
