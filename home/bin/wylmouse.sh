67f9d62264025714ab7974025d182f35641c74ee
wyl8899 <wyl8899k@gmail.com>
2017-12-31 20:33:51 +0800

    sync

--- home/bin/wylmouse.sh
+++ home/bin/wylmouse.sh
@@ -3,4 +3,5 @@ xset m default
 xset m 0
 xinput set-float-prop "Lenovo Precision USB Mouse" "Device Accel Constant Deceleration" 0.75
 xinput set-float-prop "Razer Razer DeathAdder" "Device Accel Constant Deceleration" 1.13
+xinput set-float-prop "Logitech Gaming Mouse G502" "Device Accel Constant Deceleration" 1
 xset m default
