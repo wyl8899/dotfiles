e032edf609c95332e8b36018a7c34f4d2035a395
wyl8899 <wyl8899k@gmail.com>
2019-12-20 17:05:55 +0800

    sync

--- home/bin/confxr.sh
+++ home/bin/confxr.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 set -xe
 
+# depend on the driver: nouveau or Nvidia
 # middle=""
 middle="-1"
 
-c_eDP1="--auto"
-c_DP1="--off"
-c_DP2="--off"
-if [[ "$1" == "v" ]]; then
-  c_eDP1="--off"
-  c_DP1="--off"
-  c_DP2="--auto"
-elif [[ "$1" == "m" ]]; then
-  c_eDP1="--off"
-  # c_DP2="--mode 1920x1080 --rotation normal"
-  # c_DP1="--mode 1920x1080 --rotation normal"
-  c_DP1="--mode 1920x1080 --rotation normal"
-elif [[ "$1" == "mv" ]]; then
-  c_eDP1="--off"
-  c_DP1="--mode 1920x1080 --rotation normal --pos 1080x300"
-  c_DP2="--mode 1920x1080 --rotation left --pos 0x0"
+# default: open built-in display
+OUTPUT="eDP${middle}-1"
+COMMAND="--auto"
+
+if [[ "$1" == "V" ]]; then
+  OUTPUT="eDP${middle}-1"
+  COMMAND="--off"
 fi
 
-xrandr --output eDP${middle}-1 $c_eDP1 --output DP${middle}-2 $c_DP2 --output DP${middle}-1 $c_DP1
+if [[ "$1" == "n" ]]; then
+  OUTPUT="DP${middle}-1"
+  COMMAND="--auto"
+fi
+
+if [[ "$1" == "N" ]]; then
+  OUTPUT="DP${middle}-1"
+  COMMAND="--off"
+fi
+
+if [[ "$1" == "m" ]]; then
+  OUTPUT="DP${middle}-2"
+  COMMAND="--auto"
+fi
+
+if [[ "$1" == "M" ]]; then
+  OUTPUT="DP${middle}-2"
+  COMMAND="--off"
+fi
+
+xrandr --output ${OUTPUT} ${COMMAND}
+
+# c_eDP1="--off"
+# c_DP1="--mode 1920x1080 --rotation normal --pos 1080x300"
+# c_DP2="--mode 1920x1080 --rotation left --pos 0x0"
+# xrandr --output eDP${middle}-1 $c_eDP1 --output DP${middle}-2 $c_DP2 --output DP${middle}-1 $c_DP1
--- home/bin/startup.sh
+++ home/bin/startup.sh
@@ -4,4 +4,4 @@ BIN=$HOME/bin
 $BIN/wylcaps.sh
 # $BIN/wylt450.sh
 export XDG_CURRENT_DESKTOP="Unity"
-xset r rate 200 30
+xset r rate 300 30
